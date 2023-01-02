9e8a3ba552fb55f4aebc0ae8ce0b9159f53ed2bc
veryheavypickle <xavier.carroll01@gmail.com>
2023-01-02 23:05:38 +0000

    Started config to add proper categories

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-from yodas import Menu
+from yodas import Menu, Yoda
 
 
 def main():
@@ -12,16 +12,50 @@ def main():
 
     if len(csvFiles) > 1:
         fileSelector = Menu(csvFiles, title="Choose the CSV file you wish to convert")
-        convertCSV(fileSelector.select())
+        processCSV(fileSelector.select())
     elif len(csvFiles) == 1:
-        convertCSV(csvFiles[0])
+        processCSV(csvFiles[0])
     else:
         print("No files found, drop a CSV file in the directory " + os.getcwd())
 
 
-def convertCSV(filePath):
+def processCSV(filePath):
     # format from http://homebank.free.fr/help/misc-csvformat.html
     wiseDF = pd.read_csv(filePath)
+
+    # remove duplicates of all merchants
+
+    yoda = Yoda("config")
+    extractCategoriesFromVendors(wiseDF, yoda)
+    extractDFInfo(wiseDF)
+
+
+def extractCategoriesFromVendors(wiseDF, yoda):
+    # https://whybudgeting.com/personal-budget-categories/
+    # This will create a new yoda with each vendor
+    listOfVendors = [*set(list(wiseDF["Merchant"]))]
+
+    try:
+        vendors = yoda.contents()["vendors"]
+    except KeyError:
+        vendors = {}
+    total = len(listOfVendors)
+    count = 0
+    try:
+        for vendor in listOfVendors:
+            if vendor in vendors:
+                category = vendors[vendor]
+            elif "taxi" in vendor.lower():
+                category = "Transportation:Taxi Fares"
+            else:
+                category = input("\nInput a category in format 'category:subcategory' for\n({0}/{1}){2}: ".format(count, total, vendor))
+            vendors[vendor] = category
+            count += 1
+    except KeyboardInterrupt:
+        yoda.write({"vendors": vendors})
+
+
+def extractDFInfo(wiseDF):
     wiseAsDict = wiseDF.to_dict(orient="records")
 
     homeBankDF = pd.DataFrame()
